5b3c460b684d7d7e481a772e3e654fd9c6dc649c
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2022-11-10 10:09:19 -0700

    Act.23 Implementación de plantillas con Blade Layouts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('contenido')
+    <h1>
+        Hola
+    </h1>
+@endsection
+
+
+@section('scripts')
+    <!--hola-->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
 Route::get('/login', function () {
     return view('login');
 })->name('login');
